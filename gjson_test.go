92d650367938b16990af576258bb5a52262ff495
Mindaugas Žvirblis <mz@9g.lt>
2024-04-04 06:09:19 +0000

    initial commit

---
+++ gjson_test.go
@@ -0,0 +1,42 @@
+package gjson_test
+
+import (
+	"testing"
+
+	"github.com/9glt/go-gjson"
+)
+
+type testData struct {
+	Name string `json:"name"`
+}
+
+func TestUnmarshal(t *testing.T) {
+	data := []byte(`{"name":"test"}`)
+	v, err := gjson.Unmarshal[testData](data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v.Name != "test" {
+		t.Fatalf("expected %q, got %q", "test", v.Name)
+	}
+}
+
+func TestMustUnmarshal(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("expected panic, got nil")
+		}
+	}()
+
+	data := []byte(`{"name":"test"}`)
+	v := gjson.MustUnmarshal[testData](data)
+	if v.Name != "test" {
+		t.Fatalf("expected %q, got %q", "test", v.Name)
+	}
+
+	data = []byte(`{"nameinvalid:"test"}`)
+	v = gjson.MustUnmarshal[testData](data)
+	if v.Name != "test" {
+		t.Fatalf("expected %q, got %q", "test", v.Name)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/9glt/go-gjson
+
+go 1.21.6
